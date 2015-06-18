62d72612f95cc3a77ddde61fa6631ba9d135e7b4
liqites <leechee89@gmail.com>
2015-06-18 17:20:29 +0800

    script

---
+++ leetcode/combinations.rb
@@ -0,0 +1,13 @@
+class Combination
+	# @param {Integer} n
+	# @param {Integer} k
+	# @return {Integer[][]}
+	# list all possible k-combinations
+	# given numbers from 1..n 
+	def combine(n, k)
+		result = []
+		source = Array.new(n) {|i| i+1}
+		start = source
+		
+	end
+end
