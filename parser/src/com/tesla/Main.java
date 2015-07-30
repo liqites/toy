a588aa1f45707ae11a4c3d3af237e89368f3f143
liqites <leechee89@gmail.com>
2015-07-30 15:13:53 +0800

    parser

---
+++ parser/src/com/tesla/Main.java
@@ -0,0 +1,8 @@
+package com.tesla;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
--- railsdemo/app/models/unused/record.rb
+++ railsdemo/app/models/unused/record.rb
@@ -1,21 +1,20 @@
-# class Record
-#   include ActiveSupport::Callbacks
-#   define_callbacks :save
-#   set_callback :save,:after,:ensure_run
-#   set_callback :save,:before,:parent_before
-#
-#   def save
-#     # run_callbacks :save do
-#     #   puts " - do save"
-#     # end
-#     run_callbacks :save
-#   end
-#
-#   def parent_before
-#     puts "parent_before"
-#   end
-#
-#   def ensure_run
-#     puts "parent after ..."
-#   end
-# end
+class Record
+  include ActiveSupport::Callbacks
+  define_callbacks :save
+  set_callback :save,:after,:ensure_run
+  set_callback :save,:before,:parent_before
+
+  def save
+    run_callbacks :save do
+      puts " - do save"
+    end
+  end
+
+  def parent_before
+    puts "parent_before"
+  end
+
+  def ensure_run
+    puts "parent after ..."
+  end
+end
