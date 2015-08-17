ab3e048496937907d404c5f4c51ac5a1cb6eff33
liqites <leechee89@gmail.com>
2015-08-17 17:04:48 +0800

    auto commit by shell @ 2015年 8月17日 星期一 17时04分48秒 CST

--- railsdemo/scripts/weibo_hot.rb
+++ railsdemo/scripts/weibo_hot.rb
@@ -4,7 +4,7 @@ require 'watir-webdriver'
 require 'capybara'
 require 'capybara-webkit'
 
-url = ""
+url = "http://d.weibo.com/100803?refer=index_hot_new"
 Capybara::Webkit.configure do |c|
   c.allow_url("weibo.com")
 end
@@ -13,7 +13,7 @@ Capybara.current_driver = :selenium
 Capybara.run_server = false
 browser = Capybara::Session.new(:selenium)
 browser.visit(url)
-browser.find("#").find(".m_wrap").find("ul").all("li").each do |li|
+browser.find("#Pl_Discover_Pt6Rank__5").find(".m_wrap").find("ul").all("li").each do |li|
 	info_box = li.find(".info_box")
 	title = info_box.find(".title").all("a").first.text
 	puts title
