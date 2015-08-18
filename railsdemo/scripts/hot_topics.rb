c2ccaa14d7bb576d192fa68a80ae2d6f81e2d6da
liqites <leechee89@gmail.com>
2015-08-18 17:13:31 +0800

    auto commit by shell @ 2015年 8月18日 星期二 17时13分31秒 CST

--- railsdemo/scripts/hot_topics.rb
+++ railsdemo/scripts/hot_topics.rb
@@ -2,12 +2,12 @@ require 'nokogiri'
 require 'capybara'
 require 'capybara-webkit'
 
-# url = "http://d.weibo.com/100803?refer=index_hot_new"
-path = File.absolute_path("js/a.html")
-url = "file://#{path}"
-p url
-browser = Capybara::Session.new(:poltergeist)
-p browser
-browser.visit(url)
-sleep(5);
-p browser.source
+url = "http://d.weibo.com/100803?refer=index_hot_new"
+browser = Capybara.current_session
+browser.driver.headers = {'User-Agent' => 'spider'}
+p browser.driver.headers
+browser.visit url
+browser.find("ul.pt_ul").all("li").each do |li|
+	info_box = li.find('.info_box')
+	p info_box
+end
